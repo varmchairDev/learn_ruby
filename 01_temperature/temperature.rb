af3543732073ec42f6790247a69da3843b24bf1d
Onur Kaya <onurkaya8@outlook.com>
2019-08-21 18:23:45 +0300

    first 3 challenges completed

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,15 @@
 #write your code here
+
+def ftoc(num)
+    res = (num-32)/1.8
+    round_meter = res - 0.5
+    if res.to_i > round_meter
+        return res.to_i
+    else
+        return res.to_i + 1
+    end
+end
+
+def ctof(num)
+    num*1.8 + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+
+
+def add(x,y)
+    x+y
+end
+
+def subtract(a,b)
+    a-b
+end
+
+def sum(arr)
+    res = 0
+    arr.each { |i| res += i }
+    return res
+end
+
+def multiply(x,y,*opt)
+    res = x*y
+    if opt
+        opt.each { |i| res *= i }
+    end
+    return res
+end
+
+def power(q,w)
+    q**w 
+end
+
+def factorial(n)
+    if n >= 0
+        res = 1
+        while(n>1)
+            res *= n
+            n -= 1
+        end
+        return res
+    end
+end
